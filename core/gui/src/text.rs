22f745988f9c3a6df8a92bf4fd072151c541ee09
Rahul Kumar <rahulkumar@berkeley.edu>
2025-07-07 19:21:37 -0700

    highlight text

--- core/gui/src/text.rs
+++ core/gui/src/text.rs
@@ -1,6 +1,9 @@
 use std::ops::Range;
 
-use gpui::{div, green, Context, IntoElement, ParentElement, Render, Styled, Window};
+use gpui::{
+    div, green, Context, HighlightStyle, IntoElement, ParentElement, Render, Styled, StyledText,
+    Window,
+};
 
 pub struct TextDisplay {
     pub text: String,
@@ -10,11 +13,9 @@ pub struct TextDisplay {
 impl Render for TextDisplay {
     fn render(&mut self, _window: &mut Window, _cx: &mut Context<Self>) -> impl IntoElement {
         if let Some(span) = self.highlight_span.clone() {
-            let pre = self.text[0..span.start].to_string();
-            let highlight = self.text[span.clone()].to_string();
-            let post = self.text[span.end..].to_string();
-            let inner = div().text_color(green()).child(highlight);
-            div().w_full().child(div().flex().w_full().child(pre).child(inner).child(post))
+            let highlight = HighlightStyle::color(green());
+            let text = StyledText::new(&self.text).with_highlights([(span, highlight)]);
+            div().w_full().child(text)
         } else {
             div().w_full().child(self.text.clone())
         }
