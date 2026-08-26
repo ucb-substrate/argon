de0a66bba5da4b97de147e19c08fe73e088cae8a
Rahul Kumar <rahulkumar@berkeley.edu>
2026-08-26 07:16:31 -0700

    fix(compiler): properly translate instances in bbox calculation

--- crates/compiler/src/compile.rs
+++ crates/compiler/src/compile.rs
@@ -5563,6 +5563,7 @@ impl<'a> ExecPass<'a> {
         Ok(progress)
     }
 
+    /// The bounding box of `cell`'s geometry, in `cell`'s own coordinate frame.
     pub fn bbox(&self, cell: CellId) -> Option<Rect<f64>> {
         let mut bbox = None;
         let cell = &self.compiled_cells[&cell];
@@ -5571,7 +5572,9 @@ impl<'a> ExecPass<'a> {
                 SolvedValue::Rect(r) => bbox = bbox_union(bbox, Some(r.to_float())),
                 SolvedValue::Polygon(p) => bbox = bbox_union(bbox, p.bbox()),
                 SolvedValue::Instance(i) => {
-                    let cell_bbox = self.bbox(i.cell).map(|r| r.transform(i.reflect, i.angle));
+                    let cell_bbox = self
+                        .bbox(i.cell)
+                        .map(|r| r.transform(i.reflect, i.angle).translate(i.x, i.y));
                     bbox = bbox_union(bbox, cell_bbox);
                 }
                 _ => (),
@@ -6199,6 +6202,17 @@ impl Rect<f64> {
             y1: p0p.1.max(p1p.1),
             construction: self.construction,
             span: None,
+        }
+    }
+
+    /// Translates the rect by `(dx, dy)`.
+    fn translate(&self, dx: f64, dy: f64) -> Self {
+        Self {
+            x0: self.x0 + dx,
+            y0: self.y0 + dy,
+            x1: self.x1 + dx,
+            y1: self.y1 + dy,
+            ..self.clone()
         }
     }
 }
